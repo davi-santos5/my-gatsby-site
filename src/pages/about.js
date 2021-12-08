a953cfeb378a8a10c1a836cb0ad4e622dcaa59d5
Davi Santos <davi.santos@leanonsystems.com>
2021-12-08 11:58:20 -0300

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import { Link } from "gatsby";
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => (
+  <Layout pageTitle="About Me">
+    <p>Hello there, I'm Davi, the creator of this site built with Gatsby</p>
+  </Layout>
+);
+
+export default AboutPage;
